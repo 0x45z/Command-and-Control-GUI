eeb4a287d2d3fa709a48debadc9fa6ef3f465301
copleya2 <copleya2@coventry.ac.uk>
2023-03-10 12:12:26 +0000

    Created a GUI that redirects stdout to a text widget on the right of the application. On the left are buttons for commands (not yet funcitonal). When a client connects to the controller, a new button for that client will appear. When they disconnect, the button will be removed and all buttons re-ordered. Functionality needs to be added to these buttons. Additionally, all print statements need to use threading.Lock().

--- controller.py
+++ controller.py
@@ -3,6 +3,9 @@ import threading
 import time
 import sys
 import ast
+import tkinter as tk
+
+print_lock = threading.Lock()
 
 
 class ClientHandler(threading.Thread):
@@ -32,7 +35,21 @@ class ClientHandler(threading.Thread):
             if not message:
                 # Client has disconnected
                 print(f'Client [{self.address}] Disconnected')
+
+                # get index in dict of client as this will be the index of the button that needs to be removed
+                address_list = list(server.clients.keys())
+                index = address_list.index(self.address)
+
+                # remove the button at that index in list
+                button_to_remove = app.buttons[index]
+                app.remove_client_button(button_to_remove)
+
+                # adjust the position of all other buttons
+                app.adjust_client_button_positions()
+
                 server.remove_client(self.address)  # remove client from dict
+                with print_lock:
+                    print(f"Clients Connected: {server.num_client()}")
                 self.socket.close()  # close socket
                 break  # break out of while loop
 
@@ -52,7 +69,7 @@ class Server:
 
     def listen(self):
         self.socket.listen(20)
-        print("Listening...")
+        print(f"[+] Server Listening on {self.ip}:{self.port}")
 
         try:
             while True:
@@ -62,6 +79,7 @@ class Server:
                 self.clients[address] = conn  # add the socket object to a dictionary with client address as key
                 client_thread.start()
                 print(f"Clients Connected: {len(self.clients)}")
+                app.create_client_button(address)
 
         except KeyboardInterrupt:
             self.socket.close()
@@ -72,9 +90,14 @@ class Server:
             self.clients[address].send(message)  # send message using socket object
         else:
             print(f"Client {address} does not exist")
+            for key in self.clients.keys():
+                print(key, type(key))
 
     def remove_client(self, client):
         del self.clients[client]
+
+    def num_client(self):
+        return len(self.clients)
 
 
 def one_to_one_mode():
@@ -116,6 +139,7 @@ def many_to_one_mode():
                 break
             commands = command[1:]
             commands = commands[0].split()
+            print(commands)
             addr += ")"
             addr = ast.literal_eval(addr)  # turn string to tuple
             for command in commands:
@@ -208,7 +232,7 @@ def many_to_many_mode():
                 addr = ast.literal_eval(addr)  # turn string to tuple
                 for instr in instruction:
                     server.send_to_one(addr, bytes(instr.encode('utf-8')))
-                    time.sleep(0.2)
+                    time.sleep(2)
 
         except IndexError:
             print('Invalid input')
@@ -219,30 +243,85 @@ def many_to_many_mode():
             pass
 
 
-def main():
-    # start a thread for server
-    server_thread = threading.Thread(target=server.listen)  # create a thread to listen for connections
-    server_thread.start()
-    time.sleep(0.3)  # give some time for threads to start
+class App:
+    def __init__(self, master):
+        self.master = master
 
-    # accept user input
+        # create a left frame with two columns
+        self.left_frame = tk.Frame(self.master)
+        self.left_frame.grid(row=0, column=0, rowspan=2, padx=10, pady=10, sticky="nsew")
+        self.left_frame.columnconfigure(0, weight=1)
+        self.left_frame.columnconfigure(1, weight=1)
 
-    while True:
-        try:
-            command = input("$ ")
-            if "1-1" in command:
-                one_to_one_mode()  # one command --> one address
-            elif "m-1" in command:
-                many_to_one_mode()  # many commands --> one address
-            elif "1-m" in command:
-                one_to_many_mode()  # one command --> many addresses
-            elif "m-m" in command:
-                many_to_many_mode()  # many commands --> many addresses
+        self.buttons = []  # client buttons list
 
-        except KeyboardInterrupt:
-            sys.exit(0)
+        # command buttons
+        self.button2 = tk.Button(self.left_frame, text="Get Processes")
+        self.button2.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
+
+        self.button3 = tk.Button(self.left_frame, text="Get Network Info")
+        self.button3.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
+
+        self.button4 = tk.Button(self.left_frame, text="Get Users")
+        self.button4.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
+
+        # create a canvas and text widget in the right frame
+        self.right_frame = tk.Frame(self.master)
+        self.right_frame.grid(row=0, column=1, rowspan=2, padx=10, pady=10, sticky="nsew")
+        self.right_frame.columnconfigure(0, weight=1)
+        self.right_frame.rowconfigure(1, weight=1)
+
+        self.canvas = tk.Canvas(self.right_frame, width=600, height=400, bg="white")
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+
+        self.text_widget = tk.Text(self.right_frame, width=40, height=10)
+        self.text_widget.grid(row=0, column=0, sticky="nsew")
+
+        # set the text widget to redirect stdout to the widget
+        self.text_widget.tag_configure("stdout", foreground="black")
+        sys.stdout = StdoutWriter(self.text_widget, "stdout")
+
+        # set the closing event to run the on_closing function
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def on_closing(self):
+        sys.stdout = sys.__stdout__  # restore stdout
+        self.master.destroy()  # close window
+
+    def create_client_button(self, name):
+        name = "".join(f"{name[0]}:{name[1]}")  # format client name to ip:port
+        button = tk.Button(self.left_frame, text=str(name))  # create client button
+        row_num = server.num_client() - 1  # get correct row based on number of connected clients
+        button.grid(row=row_num, column=0, padx=5, pady=5, sticky="nsew")  # set it in correct place
+        self.buttons.append(button)  # add button to buttons list
+
+    def remove_client_button(self, button):
+        # remove a client button
+        button.destroy()
+        self.buttons.remove(button)
+
+    def adjust_client_button_positions(self):
+        for i in range(len(self.buttons)):
+            # make the row of the button that of the index of the button in the list
+            self.buttons[i].grid(row=i, column=0, padx=5, pady=5, sticky="nsew")
+
+
+class StdoutWriter:
+    def __init__(self, text_widget, tag):
+        self.text_widget = text_widget
+        self.tag = tag
+
+    def write(self, msg):
+        # write stdout into tkinter widget
+        self.text_widget.configure(state="normal")
+        self.text_widget.insert("end", msg, (self.tag,))
+        self.text_widget.configure(state="disabled")
 
 
 if __name__ == "__main__":
     server = Server("127.0.0.1", 6969, 2048)
-    main()
+    root = tk.Tk()
+    app = App(root)
+    server_thread = threading.Thread(target=server.listen)  # create a thread to listen for connections
+    server_thread.start()
+    root.mainloop()
